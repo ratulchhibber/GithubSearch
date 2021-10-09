ab6ffd4a3941f3c08a871a44fe378ab0f63c3b70
Ratul Chhibber <ratulchhibber@gmail.com>
2021-10-09 20:39:14 +0530

    * Custom Utility - Loadable - It lets you switch your view accordingly via the states defined herein.

---
+++ GithubSearch/Utilities/CancelBag+Util.swift
@@ -0,0 +1,16 @@
+import Combine
+
+final class CancelBag {
+    fileprivate(set) var subscriptions = Set<AnyCancellable>()
+    
+    func cancel() {
+        subscriptions.removeAll()
+    }
+}
+
+extension AnyCancellable {
+    
+    func store(in cancelBag: CancelBag) {
+        cancelBag.subscriptions.insert(self)
+    }
+}
