7bc10f1b7809c40de33fa93ea352e95835dc8cec
Ratul Chhibber <ratulchhibber@gmail.com>
2021-10-09 13:45:31 +0530

    Initial Commit

---
+++ GithubSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubSearch
+//
+//  Created by Ratul Chhibber on 09/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubSearch/GithubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchApp.swift
+//  GithubSearch
+//
+//  Created by Ratul Chhibber on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
