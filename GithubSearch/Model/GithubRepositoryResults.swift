1a9d94aea20a01ea6a22f526024c5cd88d7d68ab
Ratul Chhibber <ratulchhibber@gmail.com>
2021-10-10 13:36:52 +0530

    * Parsing owner and createdAt keys. * Saving CreatedAt as date- Converting it to date from string via custom decoder

--- GithubSearch/Model/GithubRepositoryResults.swift
+++ GithubSearch/Model/GithubRepositoryResults.swift
@@ -5,8 +5,10 @@
 //  Created by Ratul Chhibber on 09/10/21.
 //
 
+import Foundation
+
 struct GithubRepositoryResults: Codable {
-    var totalCount: Int
+    let totalCount: Int
     var items: [GithubRepositoryResult]
 }
 
@@ -16,4 +18,30 @@ struct GithubRepositoryResult: Codable, Identifiable {
     let language: String?
     let description: String?
     let stargazersCount: Int
+    let owner: Owner?
+    let createdAt: Date?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, language, description, stargazersCount, owner, createdAt
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        language = try container.decodeIfPresent(String.self, forKey: .language)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        stargazersCount = try container.decode(Int.self, forKey: .stargazersCount)
+        owner = try container.decodeIfPresent(Owner.self, forKey: .owner)
+        if let dateString = try container.decodeIfPresent(String.self, forKey: .createdAt) {
+            createdAt = Date.dateFrom(dateString: dateString)
+        } else {
+            createdAt = nil
+        }
+    }
+}
+
+struct Owner: Codable {
+    let login: String
+    let avatarUrl: String
 }
