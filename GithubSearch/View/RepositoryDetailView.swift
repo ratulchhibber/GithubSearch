708243c74de07a055195e771abef7ba635637324
Ratul Chhibber <ratulchhibber@gmail.com>
2021-10-10 21:21:21 +0530

    * Extracting row to a ViewBuilder infoRow

--- GithubSearch/View/RepositoryDetailView.swift
+++ GithubSearch/View/RepositoryDetailView.swift
@@ -13,27 +13,34 @@ struct RepositoryDetailView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            HStack {
-                Text("Name:")
-                    .font(.title3)
-                Spacer()
-                Text(repository.name)
-            }
-            
-            HStack {
-                Text("Language:")
-                    .font(.title3)
-                Spacer()
-                Text(repository.language ?? "")
-            }
-            
-            HStack {
-                Text("⭐️")
-                    .font(.title3)
-                Spacer()
-                Text("\(repository.stargazersCount)")
-            }
+            infoRow(title: "Owner's login:",
+                    detail: repository.owner?.login ?? "")
+            infoRow(title: "Creation date:",
+                    detail: creationDate)
+            infoRow(title: "Name:",
+                    detail: repository.name)
+            infoRow(title: "Language:",
+                    detail: repository.language ?? "")
+            infoRow(title: "⭐️",
+                    detail: "\(repository.stargazersCount)")
         }
         .padding(.horizontal, 20)
+    }
+        
+    @ViewBuilder
+    private func infoRow(title: String, detail: String) -> some View {
+        HStack {
+            Text(title)
+                .font(.title3)
+            Spacer()
+            Text(detail)
+        }
+    }
+    
+    private var creationDate: String {
+        if let date = repository.createdAt {
+            return Date.stringFrom(date: date)
+        }
+        return ""
     }
 }
